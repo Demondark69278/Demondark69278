c776bfedb8042263c3cb5f99b82d7bc5639095bc
Demondark69278 <demondark69278@gmail.com>
2024-07-18 23:16:49 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,17 @@
-class Hello World
-public static void main(String[] args){
-System.out.println("Hello World");
-}
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        
+    }
 }
 <!---
 Demondark69278/Demondark69278 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
